26939bd658db6e2e42074ffa3a96bba2e5e396b5
ADRIELE-UCHOA <48423283+ADRIELE-UCHOA@users.noreply.github.com>
2019-04-08 21:21:40 -0300

    Update Test.java

--- aulas/src/listas/Test.java
+++ aulas/src/listas/Test.java
@@ -3,15 +3,23 @@ package listas;
 public class Test {
 
 	public static void main(String[] args) {
-		
+	
 		Lista lista = new Lista();
-		System.out.println("Est· vazia? " + lista.estaVazia());
-		lista.adicionar(67);
-		System.out.println("Est· vazia? " + lista.estaVazia());
-		lista.adicionar(23);
-		lista.adicionar(8);
-		lista.adicionar(0);
-		lista.adicionar(111);
+		
+		System.out.println("Est√° vazia? " + lista.estaVazia());
+		lista.adicionar(55);
+		System.out.println("Est√° vazia? " + lista.estaVazia());
+		lista.adicionar(55);
+		lista.adicionar(98);
+		lista.adicionar(12);
+		lista.adicionar(55);
+		lista.adicionar(17);
+		lista.adicionar(18);
+		lista.print();
+		lista.remove(12);
+		lista.adicionarPosicao(15, 1);
+		lista.removePosicao(2);
+		lista.removeAll(55);
 		lista.print();
 	}
 
