7658f80dc3b4805f0c04a861317db35a67bccc8c
ADRIELE-UCHOA <48423283+ADRIELE-UCHOA@users.noreply.github.com>
2019-04-09 16:33:05 -0300

    Update Pilha.java

--- aulas/src/listas/Pilha.java
+++ aulas/src/listas/Pilha.java
@@ -1,32 +1,90 @@
 package listas;
 
-public class Pilha {
-	private int[] base;
-	private int topo;
+public class Pilha{
+
+	private NoLista topo;
+	private NoLista base;
+	private int quantidade;
 	
-	public Pilha(int tamanho) {
-		this.base = new int[tamanho];
-		this.topo = -1;
+	public Pilha(){
+		this.quantidade = 0;
 	}
-	
-	public boolean estaVazia() {
-		return this.topo < 0;
+    
+	public boolean estaVazia(){
+		return this.topo == null;
 	}
-	
-	public boolean estaCheia() {
-		return this.base.length == this.topo;
+    
+	public NoLista getTopo(){
+		return this.topo;
 	}
-	
-	public void push(int valor) {
-		if(!this.estaCheia()) {
-			this.base[++topo] = valor;
+	private void setTopo(NoLista topo){
+		this.topo = topo;
+	}
+	public NoLista getBase(){
+		return this.base;
+	}
+	private void setBase(NoLista base){
+		this.base = base;
+	}
+	public int getQuantidade(){
+		return this.quantidade;
+	}
+	private void setQuantidade(int quantidade){
+		this.quantidade = quantidade;
+	}
+    
+	public void push(int valor){
+		NoLista no = new NoLista(valor);
+        
+		if(!this.estaVazia()){
+			this.topo.setNext(no);
+			this.topo = no;
+		}else{
+			this.topo = no;
+			this.base = no;
 		}
-		System.out.println("Pilha cheia!");
+		this.quantidade++;
+	}
+    
+	public void pop(){
+		NoLista current = this.base;
+        
+		if(!this.estaVazia()){
+			if(this.base != this.topo){
+				do{
+					current = current.getNext();
+				}while(current.getNext() != this.getTopo());
+				current.setNext(null);
+				this.setTopo(current);
+			}else{
+				this.base = null;
+				this.topo = null;
+			}
+		}
+		this.quantidade--;
 	}
 	
-	public void pop() {
-		if(!this.estaVazia()) {
-			--this.topo;
+	public void top(){
+		if(!this.estaVazia()){
+			System.out.println("Topo = " + this.getTopo().getValor());
 		}
+	}
+    
+	public void pull(int valor){
+		if(!this.estaVazia()){
+			this.topo.setValor(valor);
+		}
+	}
+    
+	public void print(){
+		NoLista current = this.base;
+        
+		if(!this.estaVazia()){
+			do{
+				System.out.print(current.getValor() + " ");
+				current = current.getNext();
+			}while(current != null);
+		}
+		System.out.println("");
 	}
 }
