3e7e2e3be76ef9a57b8ba8860f01adb3776a161f
ADRIELE-UCHOA <48423283+ADRIELE-UCHOA@users.noreply.github.com>
2019-04-09 16:10:31 -0300

    Update Fila.java

--- aulas/src/listas/Fila.java
+++ aulas/src/listas/Fila.java
@@ -1,19 +1,66 @@
 package listas;
 
-public class Fila {
-	private NoLista head;
-	private NoLista tail;
+public class Fila{
+	private NoLista primeiro;
+	private NoLista ultimo;
+	private int quantidade;
 	
-	public NoLista getHead() {
-		return head;
+	public Fila(){
+		this.quantidade = 0;
 	}
-	public void setHead(NoLista head) {
-		this.head = head;
+
+	public boolean estaVazia(){
+		return this.quantidade == 0;
 	}
-	public NoLista getTail() {
-		return tail;
+
+	public NoLista getPrimeiro(){
+		return this.primeiro;
 	}
-	public void setTail(NoLista tail) {
-		this.tail = tail;
+	private void setPrimeiro(NoLista primeiro){
+		this.primeiro = primeiro;
+	}
+	public NoLista getUltimo(){
+		return this.ultimo;
+	}
+	private void setUltimo(NoLista ultimo){
+		this.ultimo = ultimo;
+	}
+	public int getQuantidade(){
+		return this.quantidade;
+	}
+	private void setQuantidade(int quantidade){
+		this.quantidade = quantidade;
+	}
+
+	public void enqueue(int valor){
+		NoLista no = new NoLista(valor);
+       
+		if(!this.estaVazia()){
+			this.ultimo.setNext(no);
+			this.ultimo = no;
+		}else{
+			this.primeiro = no;
+			this.ultimo = no;
+		}
+		this.quantidade++;
+	}
+
+	public void dequeue(){
+		if(!this.estaVazia()){
+			this.primeiro = this.primeiro.getNext();
+		}
+		this.quantidade--;
+	}
+
+	public void print(){
+		NoLista current = this.primeiro;
+       
+		if(!this.estaVazia()){
+			do{
+				System.out.print(current.getValor() + " ");
+				current = current.getNext();
+			}while(current != null);
+			System.out.println("");
+		}
 	}
 }
